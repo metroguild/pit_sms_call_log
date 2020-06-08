b4902712ab1c880f99c6ff8ce18c9351e4103af0
Arif Amirani <arif@metroguild.com>
2020-06-08 23:40:38 +0530

    Call and SMS logs are sent raw as strings. Up to client to parse and pick what they want. Using startTime instead of days for granular slice

--- android/src/main/java/com/padimas/pit_sms_call_log/PitSmsCallLogPlugin.java
+++ android/src/main/java/com/padimas/pit_sms_call_log/PitSmsCallLogPlugin.java
@@ -4,21 +4,17 @@ import android.content.Context;
 import android.database.Cursor;
 import android.provider.CallLog;
 import android.provider.Telephony;
-import android.util.Log;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * PitSmsCallLogPlugin
@@ -41,22 +37,20 @@ public class PitSmsCallLogPlugin implements MethodCallHandler {
 
     @Override
     public void onMethodCall(MethodCall call, Result result) {
-        if(call.method.equals("getSmsLog")){
-
-            int days = call.argument("days");
+        if (call.method.equals("getSmsLog")) {
+            double startTime = call.argument("startTime");
             try {
-                List<Map<String, Object>> smsLogs = getSmsLog(days);
+                List<Map<String, Object>> smsLogs = getSmsLog(startTime);
                 result.success(smsLogs);
             } catch (Exception e1) {
                 e1.printStackTrace();
                 result.error("Error ", "getSmsLog Error : " + e1.getLocalizedMessage(), e1);
             }
 
-        } else if(call.method.equals("getCallLog")){
-
-            int days = call.argument("days");
+        } else if (call.method.equals("getCallLog")) {
+            double startTime = call.argument("startTime");
             try {
-                List<Map<String, Object>> callLogs = getCallLog(days);
+                List<Map<String, Object>> callLogs = getCallLog(startTime);
                 result.success(callLogs);
             } catch (Exception e) {
                 e.printStackTrace();
@@ -68,102 +62,54 @@ public class PitSmsCallLogPlugin implements MethodCallHandler {
         }
     }
 
-    private List<Map<String, Object>> getCallLog(int days) throws Exception{
+    private List<Map<String, Object>> getCallLog(final double startTime) throws Exception {
         List<Map<String, Object>> list = new ArrayList<>();
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_YEAR, - days);
-        long timeBefore = calendar.getTimeInMillis();
-        String condition = CallLog.Calls.DATE + " > " +timeBefore;
-        String [] projections = {
-                CallLog.Calls.TYPE,
-                CallLog.Calls.NUMBER,
-                CallLog.Calls.DATE,
-                CallLog.Calls.DURATION
-        };
+        DecimalFormat df = new DecimalFormat("#");
+        df.setMaximumFractionDigits(0);
+        String formattedStartTime = df.format(startTime);
 
-            Cursor cursor = context.getContentResolver().query(CallLog.Calls.CONTENT_URI, projections,
-                    condition, null, null);
+        String condition = CallLog.Calls.DATE + " > " + formattedStartTime;
+        Cursor cursor = context.getContentResolver().query(CallLog.Calls.CONTENT_URI, null,
+                condition, null, null);
 
-            if(cursor!=null){
-
-                for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
-
-                    Map<String, Object> result = new HashMap<>();
-                    for (int i = 0; i < projections.length; i++) {
-
-                        Object value;
-                        if (projections[i].equals(CallLog.Calls.TYPE)){
-                            value = getCallType(cursor.getString(i));
-                        } else if (projections[i].equals(CallLog.Calls.DATE)){
-                            value = Long.parseLong(cursor.getString(i));
-                        } else if (projections[i].equals(CallLog.Calls.DURATION)){
-                            value = Integer.parseInt(cursor.getString(i));
-                        } else {
-                            value = cursor.getString(i);
-                        }
-                        result.put(projections[i], value);
-                    }
-                    list.add(result);
+        if (cursor != null) {
+            for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+                Map<String, Object> result = new HashMap<>();
+                String[] colNames = cursor.getColumnNames();
+                for (int i = 0; i < colNames.length; i++) {
+                    Object value = cursor.getString(cursor.getColumnIndex(colNames[i]));
+                    result.put(colNames[i], value);
                 }
-                cursor.close();
+                list.add(result);
             }
-
-        return list;
-    }
-
-    private List<Map<String, Object>> getSmsLog(int days) throws Exception{
-        List<Map<String, Object>> list = new ArrayList<>();
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_YEAR, - days);
-        long timeBefore = calendar.getTimeInMillis();
-        String condition = Telephony.Sms.DATE + " > " +timeBefore;
-        String [] projections = {
-                Telephony.Sms.ADDRESS,
-                Telephony.Sms.BODY,
-                Telephony.Sms.DATE
-        };
-            Cursor cursor = context.getContentResolver().query(Telephony.Sms.CONTENT_URI, projections, condition, null, null);
-
-            if(cursor!=null){
-
-                for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
-
-                    Map<String, Object> result = new HashMap<>();
-                    for (int i = 0; i < projections.length; i++) {
-                        Object value;
-                        if(projections[i].equals( Telephony.Sms.DATE)){
-                            value = Long.parseLong(cursor.getString(i));
-                        } else {
-                            value = cursor.getString(i);
-                        }
-
-                        result.put(projections[i], value);
-                    }
-                    list.add(result);
-                }
-                cursor.close();
-            }
-
-        return list;
-    }
-
-    private String getCallType(String type){
-        String callType = "";
-        int code = Integer.parseInt(type);
-        switch (code) {
-            case CallLog.Calls.OUTGOING_TYPE:
-                callType = "OUTGOING";
-                break;
-
-            case CallLog.Calls.INCOMING_TYPE:
-                callType = "INCOMING";
-                break;
-
-            case CallLog.Calls.MISSED_TYPE:
-                callType = "MISSED";
-                break;
+            cursor.close();
         }
-        return callType;
+
+        return list;
     }
 
+    private List<Map<String, Object>> getSmsLog(double startTime) throws Exception {
+        List<Map<String, Object>> list = new ArrayList<>();
+        DecimalFormat df = new DecimalFormat("#");
+        df.setMaximumFractionDigits(0);
+        String formattedStartTime = df.format(startTime);
+
+        String condition = Telephony.Sms.DATE + " > " + startTime;
+        Cursor cursor = context.getContentResolver().query(Telephony.Sms.CONTENT_URI, null, condition, null, null);
+
+        if (cursor != null) {
+            for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+                Map<String, Object> result = new HashMap<>();
+                String[] colNames = cursor.getColumnNames();
+                for (int i = 0; i < colNames.length; i++) {
+                    Object value = cursor.getString(cursor.getColumnIndex(colNames[i]));
+                    result.put(colNames[i], value);
+                }
+                list.add(result);
+            }
+            cursor.close();
+        }
+
+        return list;
+    }
 }
